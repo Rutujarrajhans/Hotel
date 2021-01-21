152f046da9cf04db29f1fe18c33aef1f80290682
Rutujarrajhans <59066365+Rutujarrajhans@users.noreply.github.com>
2021-01-22 01:12:10 +0530

    Add files via upload

---
+++ trial/templates/me/style.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+        
+    var click=1;
+    $('.btn').click(function(){
+        var images=["url()","url()","url()",],
+        title=["Beef Burger","Beef Grills","Seafood"],
+        main_course=["1 burger200 gm","grills 250 gm","2 fish 200 gm"],
+        main1=["1 medium fries","1 salad"]
+        main2
+        main3
+        price
+    })
+   });
